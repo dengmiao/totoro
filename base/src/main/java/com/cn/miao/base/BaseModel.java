154f1fe2a1c5d4fea34984d63e33de22e80f7b61
dengmiao <1297320571@qq.com>
2019-07-23 16:02:12 +0800

    base entity

--- base/src/main/java/com/cn/miao/base/BaseModel.java
+++ base/src/main/java/com/cn/miao/base/BaseModel.java
@@ -3,10 +3,10 @@ package com.cn.miao.base;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
@@ -19,25 +19,32 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Accessors(chain = true)
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseModel<T> {
+@MappedSuperclass
+public abstract class BaseModel<T> implements Serializable {
 
+    /**
+     * 主键
+     * 非@org.springframework.data.annotation.Id
+     * 而是@javax.persistence.Id
+     * 子类可以覆盖
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private T id;
+    protected T id;
 
     @Column(name = "create_by", columnDefinition = "varchar(255) DEFAULT NULL COMMENT '创建人'")
-    private String createBy;
+    protected String createBy;
 
     @Column(name = "update_by", columnDefinition = "varchar(255) DEFAULT NULL COMMENT '更新人'")
-    private String updateBy;
+    protected String updateBy;
 
     @Column(name = "create_time", columnDefinition = "datetime COMMENT '创建时间'")
-    private LocalDateTime createTime;
+    protected LocalDateTime createTime;
 
     @Column(name = "update_time", columnDefinition = "datetime COMMENT '更新时间'")
-    private LocalDateTime updateTime;
+    protected LocalDateTime updateTime;
 
     @Column(name = "del_status", columnDefinition = "int(1) DEFAULT NULL COMMENT '删除标识位 0正常 1已删除'", length = 1)
-    private boolean delStatus;
+    protected boolean delStatus;
 }
